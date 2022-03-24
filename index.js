7179b49424ef9e7e1a2dc63be50ae75abb59ffa2
hectorplinio <hectorplinio@gmail.com>
2022-03-24 16:29:19 +0100

    Test

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const port = process.env.PORT || 3000;
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 

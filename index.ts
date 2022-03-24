5d678ebe35cad8a6bcac9fdf1a10cb44890dbacf
hectorplinio <hectorplinio@gmail.com>
2022-03-24 15:30:27 +0100

    Created folder .github and Procfile

--- index.ts
+++ index.ts
@@ -8,9 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send({
-        message: "Hello World!",
-    });
+    return res.status(200).send("<h1>Hello World!</h1>");
 });
 
 try {

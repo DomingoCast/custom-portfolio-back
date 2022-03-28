58c3d8246aba5400aa8df502e9021c7ebb06c750
Domingo Castillo <castillomartidomino@gmail.com>
2022-03-28 15:39:52 +0200

    in progress commit

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
+const AppDataSource = require("./infrastructure/postgres.datasources");
+
 const port = process.env.PORT || 3000;
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Body parsing Middleware
 app.use(express.json());
@@ -10,14 +15,24 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req: Request, res: Response): Promise<Response> => {
     return res.status(200).send({
-        message: "Hello World!",
+        message: "Hello Worlds! AWUINO CAMBIA NADA",
     });
 });
 
 try {
     app.listen(port, (): void => {
-        console.log(`Connected successfully on port ${port}`);
+        console.log("AAAAAAA");
+        console.log(`Connected successfully on portal ${port}`);
     });
 } catch (error) {
     console.error(`Error occured: ${error}`);
 }
+console.log("AAAAAAA");
+console.log("VVVVV", process.env.POSTGRES_DB);
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!");
+    })
+    .catch((error: any) => {
+        console.error("Error during Data Source initialization", error);
+    });
---
+++ src/infrastructure/postgres.datasources.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+});
+
+module.exports = AppDataSource;

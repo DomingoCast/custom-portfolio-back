9cd01162776683b5d1d80b0ef623bfd84a0ab788
Domingo Castillo <castillomartidomino@gmail.com>
2022-03-28 16:08:22 +0200

    divide app.ts in functions and add to bootstrap file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-// Bootstrap file
-const app = require("./interface/app");
-const port = process.env.PORT || 3000;
+import { runServer, createServer } from "./interface/app";
 
-try {
-    app.listen(port, (): void => {
-        console.log(`Connected successfully on port ${port}`);
-    });
-} catch (error) {
-    console.error(`Error occured: ${error}`);
-}
+const port = Number(process.env.PORT) || 3000;
+
+runServer(createServer(), port);
--- src/interface/app.ts
+++ src/interface/app.ts
@@ -1,13 +1,27 @@
+import { create } from "domain";
 import express, { Application, Request, Response } from "express";
 
-const app: Application = express();
+export const createServer = () => {
+    const app: Application = express();
 
-// Body parsing Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+    // Body parsing Middleware
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send({
-        message: "Hello World!",
+    app.get("/", async (req: Request, res: Response): Promise<Response> => {
+        return res.status(200).send({
+            message: "Hello World!",
+        });
     });
-});
+    return app;
+};
+
+export const runServer = (app: Application, port: number) => {
+    try {
+        app.listen(port, (): void => {
+            console.log(`Connected successfully on port ${port}`);
+        });
+    } catch (error) {
+        console.error(`Error occured: ${error}`);
+    }
+};

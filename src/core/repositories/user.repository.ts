3ba6469d20c07d4886d9a0a2bbd65ccd2dbe31b9
hectorplinio <hectorplinio@gmail.com>
2022-03-29 12:17:04 +0200

    Corrected all length and id type

--- src/core/repositories/user.repository.ts
+++ src/core/repositories/user.repository.ts
@@ -4,39 +4,34 @@ export const User = new EntitySchema({
     name: "User",
     columns: {
         id: {
-            type: Number,
+            type: String,
             primary: true,
             generated: true,
         },
         name: {
             type: String,
-            length: 100,
-            nullable: false,
+            length: 30,
         },
         surname: {
             type: String,
-            length: 100,
-            nullable: false,
+            length: 30,
         },
         email: {
             type: String,
             length: 100,
-            nullable: false,
+            unique: true,
         },
         password: {
             type: String,
-            length: 100,
-            nullable: false,
+            length: 30,
         },
         phone: {
             type: String,
-            length: 100,
-            nullable: false,
+            length: 30,
         },
         address: {
             type: String,
             length: 100,
-            nullable: false,
         },
     },
 });

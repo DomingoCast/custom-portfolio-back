59bba211c4fc15d62b32026bd7fb624d4b8408eb
Domingo Castillo <castillomartidomino@gmail.com>
2022-03-30 09:51:33 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req: Request, res: Response): Promise<Response> => {
     return res.status(200).send({
-        message: "Hello World!",
+        message: "Hello World prueba!",
     });
 });
 app.post(

c8ce1b5f618c81dfee6f415ce4fc82f202a3d0a1
Domingo Castillo <castillomartidomino@gmail.com>
2022-04-06 12:18:06 +0200

    stop server in test

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,8 +4,9 @@ const axios = require("axios");
 
 describe("start the server", () => {
     it("starts on 3000", async () => {
-        createServer(3000).run();
+        const server = createServer(3000).run();
         const response = await axios.get("http://localhost:3000");
         expect(response.status).toBe(200);
+        server?.close();
     });
 });

b145f171ac6d3c45e58dc4293befff64733a58d5
Domingo Castillo <castillomartidomino@gmail.com>
2022-04-11 08:11:20 +0200

    type return

--- src/infrastructure/password/create-hash-function.ts
+++ src/infrastructure/password/create-hash-function.ts
@@ -3,10 +3,13 @@ const bcrypt = require("bcryptjs");
 const createHashFunction = (): HashFunction => {
     const saltRounds = 10;
 
-    const hash = async (password: string) => {
+    const hash = async (password: string): Promise<string> => {
         return await bcrypt.hash(password, saltRounds);
     };
-    const verify = async (hashed: string, password: string) => {
+    const verify = async (
+        hashed: string,
+        password: string
+    ): Promise<boolean> => {
         return await bcrypt.compare(password, hashed);
     };
     return {

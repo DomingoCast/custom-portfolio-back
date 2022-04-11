778b3776dd5c21baa7fa929a86c83a84ddbb29c6
hectorplinio <hectorplinio@gmail.com>
2022-04-11 09:53:44 +0200

    Create swagger documentation

--- src/interface/server/app.ts
+++ src/interface/server/app.ts
@@ -2,13 +2,23 @@ import express, { Application, Request, Response } from "express";
 import { DataSource } from "typeorm";
 import registerController from "./controllers/register.controller";
 import cors from "cors";
-
+import swaggerUi from "swagger-ui-express";
+import swaggerOptions from "./api-docs/swagger-options";
 export const createServer = (port: number, dataSource: DataSource) => {
     const app: Application = express();
-
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
+    /**
+     * @openapi
+     * paths:
+     *  /:
+     *    get:
+     *      description: This endpoint is to prove that this endpoint works well.
+     *      summary: "This endpoint is to prove that this endpoint works well."
+     */
 
     app.get("/", async (req: Request, res: Response): Promise<Response> => {
         return res.status(200).send({

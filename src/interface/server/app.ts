c03c35e1975628f19b87ee0c69e2245a1e5c1787
Domingo Castillo <castillomartidomino@gmail.com>
2022-04-11 09:06:08 +0200

    remove testing

--- src/interface/server/app.ts
+++ src/interface/server/app.ts
@@ -12,7 +12,7 @@ export const createServer = (port: number, dataSource: DataSource) => {
 
     app.get("/", async (req: Request, res: Response): Promise<Response> => {
         return res.status(200).send({
-            message: "Hello World! testing 3",
+            message: "Hello World!",
         });
     });
     app.post(

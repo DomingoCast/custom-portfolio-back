d8b908dcc622f03aa877c4bf05f067d4844e5eef
Domingo Castillo <castillomartidomino@gmail.com>
2022-04-12 16:30:02 +0200

    temp

--- src/infrastructure/User/user-validate/interface/create-user-data-form-interface.ts
+++
@@ -1,9 +0,0 @@
-interface CreateUserDataForm {
-    name: string;
-    surname: string;
-    email: string;
-    password: string;
-    phone: string;
-    address: string;
-}
-export default CreateUserDataForm;

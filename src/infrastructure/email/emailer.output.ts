e00a5044ba77a6d41aa90525696195c2924abd0b
Domingo Castillo <castillomartidomino@gmail.com>
2022-04-19 10:20:32 +0200

    merge

--- src/infrastructure/email/emailer.output.ts
+++ src/infrastructure/email/emailer.output.ts
@@ -4,18 +4,10 @@ import EmailSender from "../../core/ports/send-email.port";
 import nodemailer from "nodemailer";
 
 // async..await is not allowed in global scope, must use a wrapper
-<<<<<<< HEAD:src/infrastructure/emailer.output.ts
-export const setUpEmail = (
-    service: string,
-    sender: string,
-    password: string
-): EmailSender => {
-=======
 export const setUpEmail = (): EmailSender => {
     const service = process.env.NODEMAILER_SERVICE;
     const sender = process.env.NODEMAILER_EMAIL;
     const password = process.env.NODEMAILER_PASSWORD;
->>>>>>> 904c51dac46692c71f095afe0c33d5bc8670f65d:src/infrastructure/email/emailer.output.ts
     const transporter = nodemailer.createTransport({
         service: service,
         port: Number(process.env.NODEMAILER_PORT),

6de6685c1082160ecf827b6351f03e8022a0d190
Domingo Castillo <castillomartidomino@gmail.com>
2022-04-19 09:01:45 +0200

    convert datasource to singleton

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,11 @@
 /* eslint-disable import/first */
 require("dotenv").config();
 
-import { container } from "./infrastructure/dependency-injection/awilix-set-up";
 import { createDBConnection } from "./infrastructure/persistance/postgres.datasources";
 import { createServer } from "./interface/server/app";
 
-import * as awilix from "awilix";
-
 const PORT = Number(process.env.PORT) || 3000;
 
-const { dataSource, connect } = createDBConnection();
-
-container.register({
-    dataSource: awilix.asValue(dataSource),
-});
-
-connect();
+createDBConnection().connect();
 
 createServer(PORT).run();

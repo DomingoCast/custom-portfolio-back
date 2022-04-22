65aa470929939396a0d6714639c0111acc3fde51
Domingo Castillo <castillomartidomino@gmail.com>
2022-04-22 12:26:10 +0200

    refactor

--- src/core/use-cases/user/register-user.use-case.ts
+++ src/core/use-cases/user/register-user.use-case.ts
@@ -1,15 +1,25 @@
 import { User } from "../../domain/user/user";
+import HashFunction from "../../ports/hash-function.port";
 import UserRepository from "../../ports/user-repository.port";
 
 type RegisterUserUseCaseProps = {
     userRepository: UserRepository;
+    hashFunction: HashFunction;
 };
 type RegisterUserUseCase = (user: Omit<User, "id">) => Promise<User | null>;
 
 const registerUserUseCase =
-    ({ userRepository }: RegisterUserUseCaseProps): RegisterUserUseCase =>
-    (user: Omit<User, "id">): Promise<User | null> => {
-        return userRepository.persist(user);
+    ({
+        userRepository,
+        hashFunction,
+    }: RegisterUserUseCaseProps): RegisterUserUseCase =>
+    async (user: Omit<User, "id">): Promise<User | null> => {
+        const userSafe = {
+            ...user,
+            password: await hashFunction.hash(user.password),
+        };
+
+        return userRepository.persist(userSafe);
     };
 
 export default registerUserUseCase;

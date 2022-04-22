6ba21121c14863a34aff4409f1b8f18b0ce196be
Domingo Castillo <castillomartidomino@gmail.com>
2022-04-22 11:59:37 +0200

    register controller tests

--- src/core/use-cases/user/register-user.use-case.test.ts
+++ src/core/use-cases/user/register-user.use-case.test.ts
@@ -1,3 +1,16 @@
+import registerUserUseCase from "./register-user.use-case";
+
 describe("registerUserUseCase", () => {
-    it("saves user given a user repositor", () => {});
+    it("saves user given a user repository", () => {
+        const mockResponse = "x";
+        const mockProps: any = {
+            userRepository: {
+                persist: jest.fn(() => mockResponse),
+            },
+        };
+        const mockUser: any = "x";
+        const response = registerUserUseCase(mockProps)(mockUser);
+        expect(mockProps.userRepository.persist).toBeCalledWith(mockUser);
+        expect(response).toBe(mockResponse);
+    });
 });

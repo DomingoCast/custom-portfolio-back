af2aa99ad14bf0b72359038b0ad850b8d828910b
hectorplinio <hectorplinio@gmail.com>
2022-04-29 09:52:28 +0200

    Only need type of function

--- src/infrastructure/share/trim-fields/trim-fields.ts
+++ src/infrastructure/share/trim-fields/trim-fields.ts
@@ -1,8 +1,9 @@
+import arrayExceptions from "./array-exceptions";
 const trimFields = (object: any): any => {
     const newObject = { ...object };
     Object.keys(newObject).forEach((key) => {
         if (typeof newObject[key] === "string") {
-            if (key !== "password") {
+            if (!arrayExceptions.includes(key)) {
                 newObject[key] = newObject[key].trim();
             }
         }

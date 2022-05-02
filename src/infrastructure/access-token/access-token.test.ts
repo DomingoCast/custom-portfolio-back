a5f1222e76acdc69e3cb9a3641c7bbf3b2005da1
hectorplinio <hectorplinio@gmail.com>
2022-05-02 15:27:53 +0200

    updated test

--- src/infrastructure/access-token/access-token.test.ts
+++ src/infrastructure/access-token/access-token.test.ts
@@ -5,13 +5,12 @@ describe("Test for access Token", () => {
         expect(accessToken).toBeDefined();
     });
     it("Check the function createAccessToken", () => {
-        const createAccessToken: string = accessToken().createAccessToken();
+        const createAccessToken: string = accessToken().create();
         expect(createAccessToken).toContain("");
     });
     it("Check the function verifyAccessToken", () => {
-        const createAccessToken = accessToken().createAccessToken();
-        const verifyAccessToken =
-            accessToken().verifyAccessToken(createAccessToken);
+        const createAccessToken = accessToken().create();
+        const verifyAccessToken = accessToken().verify(createAccessToken);
         expect(verifyAccessToken).toMatchObject({});
     });
 });

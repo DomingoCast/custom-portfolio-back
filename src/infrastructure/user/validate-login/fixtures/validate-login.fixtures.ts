a2de74f00c0ec6f23d55546108c42878afd7bed6
Domingo Castillo <castillomartidomino@gmail.com>
2022-05-02 12:45:46 +0200

    fix tests

--- src/infrastructure/user/validate-login/fixtures/validate-login.fixtures.ts
+++ src/infrastructure/user/validate-login/fixtures/validate-login.fixtures.ts
@@ -11,21 +11,21 @@ const fixtures = [
             email: "john@gmail",
             password: "123456",
         },
-        expected: "Invalid email",
+        expected: ["Invalid email"],
     },
     {
         login: {
             email: "john@gmail.com",
             password: "122",
         },
-        expected: "Invalid password, minimum 6 and maximum 30",
+        expected: ["Invalid password, minimum 6 and maximum 30"],
     },
     {
         login: {
             email: "john@gmail.com",
             password: "121321231223912239218482482183818248218284813",
         },
-        expected: "Invalid password, minimum 6 and maximum 30",
+        expected: ["Invalid password, minimum 6 and maximum 30"],
     },
 ];
 export default fixtures;
--- src/infrastructure/user/validate-login/validate-login.test.ts
+++ src/infrastructure/user/validate-login/validate-login.test.ts
@@ -4,7 +4,6 @@ import validateLogin from "./validate-login";
 describe("Testing of diferents type of logins", () => {
     test.each(fixtures)("Test login options", ({ login, expected }) => {
         const validate = validateLogin(login!);
-        console.log(validate, login);
         expect(validate).toStrictEqual(expected);
     });
 });

f3d087846de0b3cf1875380ec41cf316a35dddb3
Domingo Castillo <castillomartidomino@gmail.com>
2022-05-03 14:08:04 +0200

    add register admin endpoint

--- src/interface/server/app.ts
+++ src/interface/server/app.ts
@@ -7,6 +7,7 @@ import { container } from "../../infrastructure/dependency-injection/awilix-set-
 import { scopePerRequest } from "awilix-express";
 import loginController from "./controllers/login.controller";
 import CustomError from "../../infrastructure/errors/custom-error";
+import registerAdminController from "./controllers/admin/register.admin.controller";
 
 export const createServer = (port: number) => {
     const app: Application = express();
@@ -20,6 +21,8 @@ export const createServer = (port: number) => {
     app.post("/login", loginController);
 
     app.post("/register", registerController); // makeInvoker(registerController));
+
+    app.post("/admin/register", registerAdminController);
 
     return {
         app: app,

6844bb6ae5143c4263d98c12c526cddfa32cd909
Domingo Castillo <castillomartidomino@gmail.com>
2022-05-04 10:47:17 +0200

    add tests

--- src/core/use-cases/user/login-user.use-case.test.ts
+++ src/core/use-cases/user/login-user.use-case.test.ts
@@ -1,12 +1,12 @@
 import { User } from "../../domain/user/user";
 import loginUserUseCase from "./login-user.use-case";
 
-describe("Regiter user use case", () => {
+describe("Login user use case", () => {
     afterAll(() => {
         jest.clearAllMocks();
     });
 
-    it("Given a user and user repository, should return a new user", async () => {
+    it("Given a user and user repository and a hash function, if everything works it should return a new user", async () => {
         const hashPassword = "654321";
         const user: Omit<User, "id"> = {
             name: "",
@@ -25,29 +25,22 @@ describe("Regiter user use case", () => {
             id: "",
         };
         const userRepository: any = {
-            persist: jest.fn(async () => await userSafe),
-            findByEmail: jest.fn(async () => await null),
+            findByEmail: jest.fn(async () => await userSafe),
         };
         const hashFunction: any = {
-            hash: jest.fn(async () => await hashPassword),
-        };
-        const emailSender: any = {
-            send: jest.fn(),
+            verify: jest.fn(async () => await true),
         };
 
         const mockProps: any = {
             userRepository,
             hashFunction,
-            emailSender,
         };
         const result = await loginUserUseCase(mockProps)(user);
         expect(result).toStrictEqual(userSafe);
-        expect(hashFunction.hash).toHaveBeenCalled();
-        expect(userRepository.persist).toHaveBeenCalledWith(newUser);
-        expect(emailSender.send).toHaveBeenCalled();
+        expect(hashFunction.verify).toHaveBeenCalled();
+        expect(userRepository.findByEmail).toHaveBeenCalledWith(newUser.email);
     });
-
-    it("Given a user and user repository, should return a null", async () => {
+    it("Given a user and user repository and a hash function, if email in database it doesn't return", async () => {
         const hashPassword = "654321";
         const user: Omit<User, "id"> = {
             name: "",
@@ -61,27 +54,63 @@ describe("Regiter user use case", () => {
             ...user,
             password: hashPassword,
         };
-        const userSafe: null = null;
+        const userSafe: User = {
+            ...newUser,
+            id: "",
+        };
         const userRepository: any = {
-            persist: jest.fn(async () => await userSafe),
             findByEmail: jest.fn(async () => await null),
         };
         const hashFunction: any = {
-            hash: jest.fn(async () => await hashPassword),
-        };
-        const emailSender: any = {
-            send: jest.fn(),
+            verify: jest.fn(async () => await true),
         };
 
         const mockProps: any = {
             userRepository,
             hashFunction,
-            emailSender,
         };
-        const result = await loginUserUseCase(mockProps)(user);
-        expect(result).toStrictEqual(userSafe);
-        expect(hashFunction.hash).toHaveBeenCalled();
-        expect(userRepository.persist).toHaveBeenCalledWith(newUser);
-        expect(emailSender.send).not.toHaveBeenCalled();
+        let result = null;
+        try {
+            result = await loginUserUseCase(mockProps)(user);
+        } catch (err) {}
+        expect(result).toStrictEqual(null);
+        expect(userRepository.findByEmail).toHaveBeenCalledWith(newUser.email);
+    });
+    it("Given a user and user repository and a hash function, if email in database it doesn't return", async () => {
+        const hashPassword = "654321";
+        const user: Omit<User, "id"> = {
+            name: "",
+            surname: "",
+            password: "123456",
+            email: "",
+            phone: "",
+            address: "",
+        };
+        const newUser = {
+            ...user,
+            password: hashPassword,
+        };
+        const userSafe: User = {
+            ...newUser,
+            id: "",
+        };
+        const userRepository: any = {
+            findByEmail: jest.fn(async () => await userSafe),
+        };
+        const hashFunction: any = {
+            verify: jest.fn(async () => await false),
+        };
+
+        const mockProps: any = {
+            userRepository,
+            hashFunction,
+        };
+        let result = null;
+        try {
+            result = await loginUserUseCase(mockProps)(user);
+        } catch (err) {}
+        expect(result).toStrictEqual(null);
+        expect(hashFunction.verify).toHaveBeenCalled();
+        expect(userRepository.findByEmail).toHaveBeenCalledWith(newUser.email);
     });
 });

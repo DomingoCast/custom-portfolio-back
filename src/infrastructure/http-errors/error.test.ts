6c8232aac6ffc9643150073ff167878e5fa41b91
hectorplinio <hectorplinio@gmail.com>
2022-05-09 13:57:58 +0200

    change test http error

--- src/infrastructure/http-errors/error.test.ts
+++ src/infrastructure/http-errors/error.test.ts
@@ -1,20 +1,41 @@
 import HttpError from "./error";
 import BadRequestError from "./bad-request";
+import ConflictRequestError from './conflict';
+import NotFoundRequest from './not-found';
 describe("Http error test", () => {
     it("Check if HttpError is called", () => {
         const customError = new HttpError("An error had occurred", 500);
         expect(customError).toMatchObject({});
     });
-    it("Check if BadRequestError is called", () => {
-        const customError = new BadRequestError("error");
-        expect(customError).toMatchObject({});
-    });
-    it("Check if custom Error work well", () => {
+    it("Check if Http Error work well", () => {
         const t = () => {
-            throw new BadRequestError("Register Error");
+            throw new HttpError("Register Error", 500);
         };
-        expect(t).toThrowError(BadRequestError);
+        expect(t).toThrowError(HttpError);
         expect(t).toBeTruthy();
         expect(t).toThrowError("Register Error");
+    });
+});
+describe("Check all functions of HttpError", () => {
+    it("Check if Bad Request is called", () => {
+        const t = () => {
+            throw new BadRequestError("Bad Request");
+        };
+        expect(t).toThrowError(HttpError);
+        expect(t).toBeTruthy();
+    });
+    it("Check if Not found Request is called", () => {
+        const t = () => {
+            throw new NotFoundRequest("Not found Request");
+        };
+        expect(t).toThrowError(NotFoundRequest);
+        expect(t).toBeTruthy();
+    });
+    it("Check if Conflict Request is called", () => {
+        const t = () => {
+            throw new ConflictRequestError("Conflict Request");
+        };
+        expect(t).toThrowError(ConflictRequestError);
+        expect(t).toBeTruthy();
     });
 });

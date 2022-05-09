a645bb095abcdf88ece62e228c067f72e341ea5a
hectorplinio <hectorplinio@gmail.com>
2022-05-09 13:48:28 +0200

    Correct

--- src/infrastructure/http-errors/bad-request.ts
+++ src/infrastructure/http-errors/bad-request.ts
@@ -1,11 +1,8 @@
 import HttpError from "./error";
 import httpStatusCodes from "./status-codes";
 class BadRequestError extends HttpError {
-    constructor(
-        responseBody: any,
-        statusCode: number = httpStatusCodes.BAD_REQUEST
-    ) {
-        super(responseBody, statusCode);
+    constructor(responseBody: any, statusCode: number) {
+        super(responseBody, httpStatusCodes.BAD_REQUEST);
         this.name = "BadRequestError";
         Object.setPrototypeOf(this, BadRequestError.prototype);
         this.statusCode = statusCode;

db636bc3d2c340e7d263ddef0d6188c7bef9fcf7
hectorplinio <hectorplinio@gmail.com>
2022-05-17 10:17:05 +0200

    Update validate-admin.ts

--- src/interface/server/validate-admin.ts
+++ src/interface/server/validate-admin.ts
@@ -1,28 +1,24 @@
 import { Request, RequestHandler, Response, NextFunction } from "express";
 import { Role } from "../../core/domain/user/role.enum";
-import { User } from "../../core/domain/user/user";
-import jwt from "jsonwebtoken";
 import CustomError from "../../core/errors/custom-error";
+import accessToken from "../../infrastructure/access-token/access-token";
 
 const validateAdmin: RequestHandler = (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
-    const token = req.headers.token;
-    const JWT_SECRET: string = process.env.JWT_SECRET || "test";
-
-    console.log(token);
-    if (!token) throw new CustomError("No token");
-    let decoded: any = { role: Role.worker };
+    const token: string | undefined = req.headers.token as string | undefined;
+    if (!token) throw new CustomError("Token is missing");
     try {
-        decoded = <User>jwt.verify(<string>token, JWT_SECRET);
+        const decoded = accessToken().verify(token);
+        if (Number(decoded) !== Role.admin)
+            next(new CustomError("Unauthorized"));
     } catch (err) {
         console.log(err);
         next(new CustomError("Wrong Token"));
     }
-    if (Number(decoded.data.role) !== Role.admin)
-        next(new CustomError("Unauthorized"));
+
     next();
 };
 

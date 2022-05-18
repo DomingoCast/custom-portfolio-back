09186a27a1db8433691b38b99cc2a049d803eb2f
hectorplinio <hectorplinio@gmail.com>
2022-05-18 18:07:01 +0200

    Update verify.type.ts

--- src/infrastructure/access-token/verify.type.ts
+++ src/infrastructure/access-token/verify.type.ts
@@ -1,3 +1,3 @@
 import { JwtPayload } from "jsonwebtoken";
-export type AccessTokenResponse = string | Error | undefined;
-export type VerifyResponse = string | JwtPayload | undefined | Error;
+export type AccessTokenResponse = string | undefined;
+export type VerifyResponse = string | JwtPayload | undefined;

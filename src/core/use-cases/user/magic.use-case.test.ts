091bff5f72f33cad1c4a6abcf893a01a26b05bd9
Domingo Castillo <castillomartidomino@gmail.com>
2022-05-18 11:12:30 +0200

    tests

---
+++ src/core/use-cases/user/magic.use-case.test.ts
@@ -0,0 +1,42 @@
+import { toEditorSettings } from "typescript";
+import UnauthorizedError from "../../errors/unauthorized.error";
+import { magicUseCase } from "./magic.use-case";
+
+describe("magic admin controller", () => {
+    it("Given a token function and a token, and the token doesn't have change password, returns true", () => {
+        const accessToken: any = {
+            verify: jest.fn(() => ({
+                data: {},
+            })),
+        };
+        const token = "";
+        const response = magicUseCase({ accessToken })(token);
+        expect(accessToken.verify).toHaveBeenCalledWith(token);
+        expect(response).toBe(true);
+    });
+    it("Given a token function and a token, and the token does have change password, returns true", () => {
+        const accessToken: any = {
+            verify: jest.fn(() => ({
+                data: { changePassword: true },
+            })),
+        };
+        const token = "";
+        const response = magicUseCase({ accessToken })(token);
+        expect(accessToken.verify).toHaveBeenCalledWith(token);
+        expect(response).toBe(false);
+    });
+    it("Given a token function and a token, and the token function throws error, a UnauthorizedError is thrown", () => {
+        const accessToken: any = {
+            verify: jest.fn(() => {
+                throw new Error();
+            }),
+        };
+        const token = "";
+        try {
+            const response = magicUseCase({ accessToken })(token);
+        } catch (error) {
+            expect(error).toBeInstanceOf(UnauthorizedError);
+        }
+        expect(accessToken.verify).toHaveBeenCalledWith(token);
+    });
+});
--- src/interface/server/controllers/admin/magic.admin.controller.test.ts
+++ src/interface/server/controllers/admin/magic.admin.controller.test.ts
@@ -1,14 +1,15 @@
 import magicAdminController from "./magic.admin.controller";
 
 describe("magic admin controller", () => {
-    it("responds with status 200 when decoded token is false", () => {
+    it("responds with status 200 when decoded magicUseCase returns true", () => {
         const container = {
             cradle: {
                 accessToken: {
                     verify: jest.fn(() => ({
-                        data: { changePassword: false },
+                        data: { changePassword: true },
                     })),
                 },
+                magicUseCase: jest.fn(() => true),
             },
         };
         const req: any = { query: { token: "" }, container };
@@ -21,12 +22,15 @@ describe("magic admin controller", () => {
         magicAdminController(req, res, next);
         expect(res.status).toHaveBeenCalledWith(200);
     });
-    it("returns error given a token whick contains changePassword", () => {
+    it("responds with status 401 when decoded magicUseCase returns false", () => {
         const container = {
             cradle: {
                 accessToken: {
-                    verify: jest.fn(() => ({ data: { changePassword: true } })),
+                    verify: jest.fn(() => ({
+                        data: { changePassword: false },
+                    })),
                 },
+                magicUseCase: jest.fn(() => false),
             },
         };
         const req: any = { query: { token: "" }, container };
@@ -37,6 +41,6 @@ describe("magic admin controller", () => {
         };
         const next = jest.fn;
         magicAdminController(req, res, next);
-        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.status).toHaveBeenCalledWith(401);
     });
 });

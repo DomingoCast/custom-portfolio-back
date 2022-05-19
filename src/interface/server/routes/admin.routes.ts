41a2d2057b006d34ebdb65cdfa339d25d2e66db6
Domingo Castillo <castillomartidomino@gmail.com>
2022-05-19 10:33:29 +0200

    add admin/password route

--- src/interface/server/routes/admin.routes.ts
+++ src/interface/server/routes/admin.routes.ts
@@ -1,8 +1,10 @@
 import express, { Router } from "express";
+import passwordAdminController from "../controllers/admin/password.admin.controller";
 import registerAdminController from "../controllers/admin/register.admin.controller";
 const router: Router = express.Router();
 const adminRouter = (): Router => {
     router.post("/register", registerAdminController);
+    router.post("/password", passwordAdminController);
     return router;
 };
 export default adminRouter;

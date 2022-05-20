e741fba702d5a228077655b1fcee4ed6c0792595
Domingo Castillo <castillomartidomino@gmail.com>
2022-05-20 08:44:33 +0200

    type magic use case test

--- src/core/use-cases/user/magic.use-case.test.ts
+++ src/core/use-cases/user/magic.use-case.test.ts
@@ -1,12 +1,15 @@
+import { LoginInfo } from "../../domain/user/login-info";
 import UnauthorizedError from "../../errors/unauthorized.error";
+import AccessToken from "../../ports/access-token.port";
 import { magicUseCase } from "./magic.use-case";
 
 describe("magic admin controller", () => {
     it("Given a token function and a token, and the token doesn't have change password, returns true", () => {
-        const accessToken: any = {
+        const accessToken: AccessToken = {
             verify: jest.fn(() => ({
                 data: {},
             })),
+            create: (loginInfo: Omit<LoginInfo, "password">) => "",
         };
         const token = "";
         const response = magicUseCase({ accessToken })(token);

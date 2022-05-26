6e3bd3946720c02ae10d7268b362252b79d36251
Domingo Castillo <castillomartidomino@gmail.com>
2022-05-26 10:23:55 +0200

    test

---
+++ src/interface/server/wrappers/controller.wrapper.test.ts
@@ -0,0 +1,46 @@
+import { Response } from "@sendgrid/helpers/classes";
+import CustomError from "../../../core/errors/custom-error";
+import httpHandlerError from "../../../infrastructure/http-errors/http-error-handler";
+import { CustomRequest } from "../types/custom.request";
+import { controllerWrapper } from "./controller.wrapper";
+
+jest.mock("../../../infrastructure/http-errors/http-error-handler", () =>
+    jest.fn()
+);
+
+const mockHttpHandlerError = httpHandlerError as unknown as jest.Mock;
+describe("controller wrapper", () => {
+    const req: any = {
+        body: null,
+        container: {
+            cradle: {
+                logger: { error: jest.fn, info: jest.fn },
+            },
+        },
+    };
+    const res: any = {
+        status: jest.fn(() => ({
+            send: jest.fn,
+        })),
+    };
+    const next: any = jest.fn();
+    mockHttpHandlerError.mockImplementation(jest.fn());
+    it("calls handler error if controller throws CustomError", () => {
+        mockHttpHandlerError.mockReset();
+        const controller: any = (req: CustomRequest, res: Response) => {
+            throw new CustomError();
+        };
+        controllerWrapper(controller)(req, res, next);
+        expect(mockHttpHandlerError).toHaveBeenCalled();
+    });
+    it("calls next if controller doesn't throw an error instance of CustomError", () => {
+        mockHttpHandlerError.mockReset();
+        mockHttpHandlerError.mockImplementation(jest.fn());
+        const controller: any = (req: CustomRequest, res: Response) => {
+            return "";
+        };
+        controllerWrapper(controller)(req, res, next);
+        expect(mockHttpHandlerError).not.toHaveBeenCalled();
+        expect(next).toHaveBeenCalled();
+    });
+});
